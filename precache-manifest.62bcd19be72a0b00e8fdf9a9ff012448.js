83ed26b4eded1a065f8a500b0b93c2ed3e1fa7e9
Maanas Muddam <maanasnandu@gmail.com>
2019-07-03 09:49:37 +0530

    Updates

---
+++ precache-manifest.62bcd19be72a0b00e8fdf9a9ff012448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffefe64125d12da0e6df97e3e68a561f",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "fd5bea5b231998ebc54e",
+    "url": "/client/static/css/main.b0d3335a.chunk.css"
+  },
+  {
+    "revision": "62bfd6d98676a911ba78",
+    "url": "/client/static/js/2.2bdf8ce5.chunk.js"
+  },
+  {
+    "revision": "fd5bea5b231998ebc54e",
+    "url": "/client/static/js/main.3370023a.chunk.js"
+  },
+  {
+    "revision": "79f1a12bb5d1cdf91f65",
+    "url": "/client/static/js/runtime~main.3fca2621.js"
+  }
+]);
